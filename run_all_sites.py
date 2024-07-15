82ef96140ce5b1e8facdba9659b4c55db8ef148f
Charlie Marshak <charlie.z.marshak@jpl.nasa.gov>
2024-07-15 14:48:27 -0700

    combine papermill params

--- run_all_sites.py
+++ run_all_sites.py
@@ -16,18 +16,19 @@ def main():
     df_val_bursts = gpd.read_parquet('data/validation_bursts_v1_coverage_updated.parquet')
 
     site_ids = df_val_bursts.site_id.unique().tolist()
+    df_count = df_val_bursts.groupby('site_id').size().rename(index='bursts').reset_index(drop=False)
+    site_ids, counts = df_count.site_id.tolist(), df_count.bursts.tolist()
+    parameters = [(site_id, burst_idx) for (site_id, count) in zip(site_ids, counts) for burst_idx in range(count)]
 
-    for site_id in tqdm(site_ids, desc='sites'):
-        df_bursts_for_site = df_val_bursts[df_val_bursts.site_id == site_id].reset_index(drop=True)
-        n = df_bursts_for_site.shape[0]
-        for burst_idx in range(n):
-            print(f'{site_id=}', f'{burst_idx=}')
-            out_nb = ipynb_dir / f'{site_id}_{burst_idx}.ipynb'
-            pm.execute_notebook(in_nb,
-                                output_path=out_nb,
-                                    parameters=dict(SITE_ID=site_id,
-                                                    IDX_BURST=burst_idx)
-                                )
+    for param in tqdm(parameters, desc='sites/bursts'):
+        site_id, burst_idx = param
+        print(f'{site_id=}', f'{burst_idx=}')
+        out_nb = ipynb_dir / f'{site_id}_{burst_idx}.ipynb'
+        pm.execute_notebook(in_nb,
+                            output_path=out_nb,
+                                parameters=dict(SITE_ID=site_id,
+                                                IDX_BURST=burst_idx)
+                            )
 
 if __name__ == '__main__':
     main()
