02690796877eef5978ce02157682c28cf562e11a
Charlie Marshak <charlie.z.marshak@jpl.nasa.gov>
2024-09-27 16:51:53 -0700

    reorganize

--- run_all_sites.py
+++ run_all_sites.py
@@ -6,45 +6,101 @@ import papermill as pm
 from tqdm import tqdm
 
 
-@click.option('--param_idx',
-              required=False,
-              type=int,
-              default=0,
-              help='Restart on param_idx given')
-@click.command()
-def main(param_idx: int):
-    in_nb = Path("ts-explore-by-site-stream.ipynb")
-    p = "out_notebooks"
-    ipynb_dir = Path(p)
-    ipynb_dir.mkdir(exist_ok=True, parents=True)
+DISTMETRIC_NAMES = [
+    "transformer",
+    "mahalanobis_2d",
+    "mahalanobis_vh",
+    "mahalanobis_1d_max",
+    "log_ratio_vh",
+    "cusum_vh",
+    "cusum_prob_max",
+]
 
+
+def get_site_burst_count():
     df_val_bursts = gpd.read_parquet(
         "data/validation_bursts_v1_coverage_updated.parquet"
     )
 
-    site_ids = df_val_bursts.site_id.unique().tolist()
-    df_count = (
+    df_site_burst_count = (
         df_val_bursts.groupby("site_id")
         .size()
         .rename(index="bursts")
         .reset_index(drop=False)
     )
+    return df_site_burst_count
+
+
+@click.option(
+    "--param_idx_start",
+    required=False,
+    type=int,
+    default=0,
+    help="which parameter index to start (site_id/burst_idx combo) for all steps",
+)
+@click.option(
+    "--n_cpus",
+    required=False,
+    type=int,
+    default=5,
+    help="which parameter index to start (site_id/burst_idx combo)",
+)
+@click.option(
+    "--step_start", required=False, type=int, default=1, help="Which Notebook to start"
+)
+@click.command()
+def main(param_idx_start: int, n_cpus: int, step_start: int):
+    in_nbs = [Path("1_Localize_RTC_Data.ipynb"), Path("2_Localize_Metric_tables.ipynb")]
+
+    p = "out_notebooks"
+    ipynb_dir = Path(p)
+    ipynb_dir.mkdir(exist_ok=True, parents=True)
+
+    df_count = get_site_burst_count()
     site_ids, counts = df_count.site_id.tolist(), df_count.bursts.tolist()
     parameters = [
         (site_id, burst_idx)
         for (site_id, count) in zip(site_ids, counts)
         for burst_idx in range(count)
     ]
-
-    for param_idx, param in enumerate(tqdm(parameters[param_idx:], desc="sites/bursts")):
-        site_id, burst_idx = param
-        print(f"{param_idx=}", f"{site_id=}", f"{burst_idx=}")
-        out_nb = ipynb_dir / f"{site_id}_{burst_idx}.ipynb"
-        pm.execute_notebook(
-            in_nb,
-            output_path=out_nb,
-            parameters=dict(SITE_ID=site_id, IDX_BURST=burst_idx),
-        )
+    parameters = [
+        (param_idx, site_id, count)
+        for param_idx, (site_id, count) in enumerate(parameters)
+    ]
+    # Localize RTC Data
+    if param_idx_start:
+        parameters = parameters[param_idx_start:]
+    nb_idx = step_start - 1
+    for in_nb in in_nbs[nb_idx:]:
+        step_ipynb_dir = ipynb_dir / in_nb.stem
+        step_ipynb_dir.mkdir(exist_ok=True, parents=True)
+        for param_idx, site_id, burst_idx in tqdm(
+            parameters, desc="sites/bursts combos"
+        ):
+            print(f"{param_idx=}", f"{site_id=}", f"{burst_idx=}")
+            out_nb = (
+                step_ipynb_dir
+                / f"{param_idx}__site{site_id}_burstidx{burst_idx}_{in_nb.stem[2:]}.ipynb"
+            )
+            if "1_" in in_nb.stem:
+                pm.execute_notebook(
+                    in_nb,
+                    output_path=out_nb,
+                    parameters=dict(
+                        SITE_ID=site_id, IDX_BURST=burst_idx, N_CPUS=n_cpus
+                    ),
+                )
+            if "2_" in in_nb.stem:
+                for distmetric_name in DISTMETRIC_NAMES[:1]:
+                    pm.execute_notebook(
+                        in_nb,
+                        output_path=out_nb,
+                        parameters=dict(
+                            SITE_ID=site_id,
+                            IDX_BURST=burst_idx,
+                            DISTMETRIC_NAME=distmetric_name,
+                        ),
+                    )
 
 
 if __name__ == "__main__":
--- val_io.py
+++ val_io.py
@@ -6,6 +6,7 @@ from pyproj import Transformer
 from rasterio import RasterioIOError
 from rasterio.crs import CRS
 from rasterio.transform import rowcol
+from rasterio.transform import xy
 from rasterio.windows import Window
 from requests.exceptions import HTTPError
 
@@ -17,10 +18,8 @@ from requests.exceptions import HTTPError
     max_time=60,
     jitter=backoff.full_jitter,
 )
-def get_utm_coords(url: str, lon: float, lat: float) -> tuple[float, float]:
-    with rasterio.open(url) as src:
-        src_crs = src.crs
-    transformer = Transformer.from_crs(CRS.from_epsg(4326), src_crs, always_xy=True)
+def get_utm_coords(target_utm_crs: CRS, lon: float, lat: float) -> tuple[float, float]:
+    transformer = Transformer.from_crs(CRS.from_epsg(4326), target_utm_crs, always_xy=True)
     utm_x, utm_y = transformer.transform(lon, lat)
     return utm_x, utm_y
 
@@ -35,28 +34,41 @@ def get_utm_coords(url: str, lon: float, lat: float) -> tuple[float, float]:
 def read_window_from_raster(
     url: str, utm_x: float, utm_y: float, window_size=3
 ) -> np.ndarray:
-    with rasterio.open(url) as src:
-        src_trans = src.transform
-
     if (window_size % 2) != 1:
         raise ValueError("window size must be odd")
+    with rasterio.open(url) as ds:
+        src_trans = ds.transform
     row, col = rowcol(src_trans, utm_x, utm_y)
     row_m1 = max(row - window_size // 2, 0)
     col_m1 = max(col - window_size // 2, 0)
     window = Window(col_m1, row_m1, window_size, window_size)
 
-    with rasterio.open(url) as src:
-        values = src.read(1, window=window)
-    return values
+    with rasterio.open(url) as ds:
+        arr = ds.read(window=window)
+        t_window = ds.window_transform(window)
+        p_ref = ds.profile
+
+    prof = p_ref.copy()
+    prof['transform'] = t_window
+    prof['height'], prof['width'] = arr.shape[-2], arr.shape[-1]
+
+    return arr, prof
 
 
 @backoff.on_exception(
-    backoff.expo, (RuntimeError, OSError), max_tries=20, max_time=60, jitter=backoff.full_jitter
+    backoff.expo,
+    (RuntimeError, OSError),
+    max_tries=20,
+    max_time=60,
+    jitter=backoff.full_jitter,
 )
 def get_burst_time_series_around_point(
     urls: list[str], lon_site, lat_site, window_size=3, n_workers: int = 10
 ) -> list[np.ndarray]:
-    utm_x, utm_y = get_utm_coords(urls[0], lon_site, lat_site)
+
+    with rasterio.open(urls[0]) as ds:
+        utm_crs = ds.crs
+    utm_x, utm_y = get_utm_coords(utm_crs, lon_site, lat_site)
 
     def read_window_from_raster_p(url: str):
         return read_window_from_raster(url, utm_x, utm_y, window_size=window_size)
@@ -65,7 +77,8 @@ def get_burst_time_series_around_point(
         vals_l = pool.map(
             read_window_from_raster_p,
             urls,
-            progress_bar=True, progress_bar_style='std',
+            progress_bar=True,
+            progress_bar_style="std",
             concatenate_numpy_output=False,
         )
     return vals_l
